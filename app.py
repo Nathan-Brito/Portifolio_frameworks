831d8eac3e97d136ba19a250d9010e8e2eda615f
Nathan-Brito <nathansilvabrito@hotmail.com>
2025-02-04 23:16:59 -0400

    Corrigindo para Subir

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from atividades.app import atividades_blueprint
 from aulas.app import aulas_blueprint
 from flask_scss import Scss
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def index():
     return render_template('index.html') 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True) 
